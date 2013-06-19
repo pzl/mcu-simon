3c5401f864b5d119b8908bb1231e56fa77926a02
pzl <dan@panzarel.la>
2013-06-18 23:32:29 -0400

    dancing lights and beginnings of a round

--- simon.c
+++ simon.c
@@ -1,15 +1,17 @@
 /*
 simon.c
-test program for attiny2313a on stk500v2 dev board
-let's make the lights dance
+let's make a memory game!
 */
 
 
 #include <avr/io.h>
+#include <stdlib.h>
 
-#define wait 60
+#define QUICK 60
+#define MED 330
+#define SLOW 400
 
-void delay_ms(uint8_t ms) {
+void delay_ms(uint16_t ms) {
 	uint16_t delay_count = 1000000 / 17500;
 	volatile uint16_t i;
 
@@ -24,29 +26,65 @@ void dance(void){
 
 	for (i=0; i<8; i++){
 		PORTB = ~(1<<i);
-		delay_ms(wait);
+		delay_ms(QUICK);
 	}
 	for (i=8; i>0; i--){
 		PORTB = ~(1<<(i-1));
-		delay_ms(wait);
+		delay_ms(QUICK);
 	}
 }
 
 void blink(void) {
 	PORTB = 0;
-	delay_ms(wait);
+	delay_ms(QUICK);
 	PORTB = 0xFF;
-	delay_ms(wait);
+	delay_ms(QUICK);
 }
 
-int main(void) {
-	DDRB = 0xFF; // ALL the outputs
-	PORTB = 0xFF; //start off
-
+void intro(void) {
+	PORTB = 0xFF; // begin off
 	dance();
 	blink();
 	blink();
 	PORTB = 0xFF; //end off
+}
+
+
+
+int stage(int level) {
+	uint8_t a, b, i;
+	b = 1;
+
+	for (i = level; i>0; i--) {
+		a = rand() % 8;
+		PORTB = ~(1<<a);
+		delay_ms(MED);
+		PORTB = 0xFF;
+		while (b){
+			if (bit_is_clear()) {
+				
+				b=0;
+			}
+		}
+
+	}
+	return 1;
+}
+
+int main(void) {
+	uint8_t lvl;
+	lvl = 1;
+
+	DDRB = 0xFF; // ALL the outputs
+	PORTB = 0xFF; //start off
+
+	DDRD = 0; //all the.. inputs!
+	PORTD = 0xFF; //pull ups everywhere. DYEL?
+	srand(500);
+
+	intro();
+	delay_ms(SLOW);
+	stage(lvl);
 	while (1){
 	}
 }
